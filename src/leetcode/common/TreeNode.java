54f5f57dd8e2121dfcf34b7fc149f7a9b4a640c7
Feiyu Shi <flyingcatok@gmail.com>
2015-02-17 17:19:04 -0500

    tree node class for binary tree.

---
+++ src/leetcode/common/TreeNode.java
@@ -0,0 +1,16 @@
+package leetcode.common;
+
+/**
+ * Definition for binary tree node.
+ * @author feiyu
+ * @since February 17, 2015
+ */
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	
+	public TreeNode(int x){
+		val = x;
+	}
+}
