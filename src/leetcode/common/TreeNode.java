e665c87d924a2699d3dcd61d08eea651b8bc4252
Feiyu Shi <flyingcatok@gmail.com>
2015-03-07 17:12:01 -0500

    add junk code: toString()

--- src/leetcode/common/TreeNode.java
+++ src/leetcode/common/TreeNode.java
@@ -1,5 +1,8 @@
 package leetcode.common;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for binary tree node.
  * @author feiyu
@@ -13,4 +16,30 @@ public class TreeNode {
 	public TreeNode(int x){
 		val = x;
 	}
+	
+//	public String toString(){
+//		StringBuilder builder = new StringBuilder();
+//		Queue<TreeNode> queue = new LinkedList<>();
+//		TreeNode temp = this;
+//		while(temp != null){
+//			if(temp.val == Integer.MAX_VALUE){
+//				builder.append("#,");
+//			}
+//			builder.append(temp.val+",");
+//			if(temp.left!=null){
+//				queue.add(temp.left);
+//			}else{
+//				queue.add(new TreeNode(Integer.MAX_VALUE));
+//			}
+//			if(temp.right!=null){
+//				queue.add(temp.right);
+//			}else{
+//				queue.add(new TreeNode(Integer.MAX_VALUE));
+//			}
+//			
+//			temp = queue.poll();
+//		}
+//		return builder.toString();
+//		
+//	}
 }
