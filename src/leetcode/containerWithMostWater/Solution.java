49f704b4be8b10faf54d67f885d2b5b7280821d7
Feiyu Shi <flyingcatok@gmail.com>
2015-03-09 17:51:11 -0400

    problem 11: little improvement

--- src/leetcode/containerWithMostWater/Solution.java
+++ src/leetcode/containerWithMostWater/Solution.java
@@ -7,8 +7,11 @@ package leetcode.containerWithMostWater;
  */
 public class Solution {
 	public static int maxArea(int[] height) {
-        int area = 0;
         int n = height.length;
+        if(height == null || n < 2){
+        	return 0;
+        }
+        int area = 0;
         int p1=0, p2=n-1;
         while(p1<p2){
             area = Math.max(Math.min(height[p1],height[p2])*(p2-p1),area);
