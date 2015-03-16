48fd59c0c1ad255e17710081773709ba4190a352
Feiyu Shi <flyingcatok@gmail.com>
2015-03-16 15:22:36 -0400

    update the test case for binary tree inorder and preorder traversal

--- src/leetcode/binaryTreeInorderTraversal/Problem94.java
+++ src/leetcode/binaryTreeInorderTraversal/Problem94.java
@@ -16,6 +16,10 @@ public class Problem94 {
 		TreeNode root = new TreeNode(1);
 		root.left = new TreeNode(2);
 		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(7);
 		
 		List<Integer> list = new ArrayList<>();
 		list  = Solution3.inorderTraversal(root);
--- src/leetcode/binaryTreePreorderTraversal/Problem144.java
+++ src/leetcode/binaryTreePreorderTraversal/Problem144.java
@@ -15,6 +15,12 @@ public class Problem144 {
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1);
 		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(7);
+		
 		List<Integer> list = new ArrayList<>();
 		list  = Solution2.preorderTraversal(root);
 		for(int i =0; i<list.size();i++){
