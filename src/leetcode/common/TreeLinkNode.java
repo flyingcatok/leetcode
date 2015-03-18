7028edb99815615c9031c069f1b1225710df5e42
Feiyu Shi <flyingcatok@gmail.com>
2015-03-18 14:29:40 -0400

    add tree link node definition

---
+++ src/leetcode/common/TreeLinkNode.java
@@ -0,0 +1,17 @@
+package leetcode.common;
+
+/**
+ * Definition for binary tree with next pointer.
+ * @author feiyu
+ * @since March 18, 2015
+ */
+public class TreeLinkNode {
+	public int val;
+	public TreeLinkNode left;
+	public TreeLinkNode right;
+	public TreeLinkNode next;
+	
+	public TreeLinkNode(int x){
+		val = x;
+	}
+}
