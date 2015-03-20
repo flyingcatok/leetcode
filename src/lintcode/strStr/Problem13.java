4c95ba0dcb8cb7d65cf4d0aa47a74a1dd095f70c
Feiyu Shi <flyingcatok@gmail.com>
2015-03-20 13:51:07 -0400

    line problem 13: find a substring in a string

---
+++ src/lintcode/strStr/Problem13.java
@@ -0,0 +1,16 @@
+package lintcode.strStr;
+
+/**
+ * 
+ * @author feiyu
+ * @since March 20, 2015
+ */
+public class Problem13 {
+
+	public static void main(String[] args) {
+		String source = "abcdabcdefg";
+		String target = "bcd";
+		System.out.println(Solution.strStr(source, target));
+	}
+
+}
