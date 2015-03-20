6f96c3b2f4db123de7ea8dfd38cc3da7a7ded1ed
Feiyu Shi <flyingcatok@gmail.com>
2015-03-20 14:23:46 -0400

    problem 28: add comment

--- src/leetcode/strStr/Problem28.java
+++ src/leetcode/strStr/Problem28.java
@@ -1,5 +1,11 @@
 package leetcode.strStr;
 
+/**
+ * Implement strStr().
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * @author feiyu
+ * @since March 20, 2015
+ */
 public class Problem28 {
 
 	public static void main(String[] args) {
