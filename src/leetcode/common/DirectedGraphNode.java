d401f70f98fb20118ea636d20f4620c0976441fe
Feiyu Shi <flyingcatok@gmail.com>
2015-05-06 14:44:33 -0400

    definition: directed graph node.

---
+++ src/leetcode/common/DirectedGraphNode.java
@@ -0,0 +1,18 @@
+package leetcode.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for directed graph node.
+ * @author feiyu
+ * @since May 6, 2015
+ */
+public class DirectedGraphNode {
+	public int label;
+	public List<DirectedGraphNode> neighbors;
+	public DirectedGraphNode (int x) {
+		label = x;
+		neighbors = new ArrayList<DirectedGraphNode>();
+	}
+}
